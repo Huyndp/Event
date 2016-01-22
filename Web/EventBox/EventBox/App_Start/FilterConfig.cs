164f8971ba3e3dcdc0e21ad1e876993bdec03d79
Huyndp <huyndp@gmail.com>
2016-01-22 16:09:17 +0700

    Add Homepage

---
+++ Web/EventBox/EventBox/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EventBox
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
